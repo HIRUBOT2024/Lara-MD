b6b05647338cf54797c9fd04e8cd692249af29f8
HIRUBOT2024 <janakasiribandara19780630@gmail.com>
2024-12-19 07:36:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "y35FCRgK#j_31TA7DsgzNFF2Vi7GqFgJS4bS-6n_j4leif6T5qKk", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
